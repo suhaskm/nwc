3bd168e92bf99521192251b9deec9c1c2774fdad
suhaskm <1987kmsuhas@gmail.com>
2019-04-01 03:08:58 +0530

    Add files via upload
    adding solution and helper tests

---
+++ NWC.Helper.Layer.Tests/ConfigurationHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NWC.Helper.Layer.Tests
+{
+    [TestClass]
+    public class ConfigurationHelperTests
+    {
+        [TestMethod]
+        public void GetConfigurations_Pass()
+        {
+            var sut = ConfigurationHelper.GetConfigurations("AllowNegative");
+            Assert.AreEqual(sut, "true");
+        }
+
+        [TestMethod]
+        public void GetConfigurations_Fail()
+        {
+            var sut = ConfigurationHelper.GetConfigurations("NoSuchKey");
+            Assert.AreEqual(sut, string.Empty);
+        }
+    }
+}
---
+++ NWC.Helper.Layer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NWC.Helper.Layer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NWC.Helper.Layer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d75a69d5-4a53-4e36-a775-c07cd41788c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
